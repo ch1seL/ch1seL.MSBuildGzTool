5e6b8532dfda721c8b075c6a5184239880e6cd5b
Alexander Salamatov <ch1seL@ya.ru>
2020-11-30 21:22:13 +0300

    init

---
+++ src/ch1seL.MSBuildGzTool/Gzip.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.IO.Compression;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ch1seL.MSBuildGzTool
+{
+    public class Gzip : Task
+    {
+        public override bool Execute()
+        {
+            using var fStream = new FileStream(SourceFile,
+                FileMode.Create, FileAccess.Write);
+            using var zipStream = new GZipStream(fStream,
+                CompressionMode.Compress);
+            var inputFile = File.ReadAllBytes(DestinationFile);
+            zipStream.Write(inputFile, 0, inputFile.Length);
+
+            return true;
+        }
+        
+        // ReSharper disable once MemberCanBePrivate.Global
+        [Required]
+        public string SourceFile { get; set; }
+        [Required]
+        // ReSharper disable once MemberCanBePrivate.Global
+        public string DestinationFile { get; set; }
+    }
+}
---
+++ src/ch1seL.MSBuildGzTool/UnGzip.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.IO.Compression;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ch1seL.MSBuildGzTool
+{
+    public class UnGzip : Task
+    {
+        public override bool Execute()
+        {
+            using var fInStream = new FileStream(SourceFile, FileMode.Open, FileAccess.Read);
+            using var zipStream = new GZipStream(fInStream, CompressionMode.Decompress);
+            using var fOutStream = new FileStream(DestinationFile, FileMode.Create, FileAccess.Write);
+            var tempBytes = new byte[4096];
+            int i;
+            while ((i = zipStream.Read(tempBytes, 0, tempBytes.Length)) != 0) fOutStream.Write(tempBytes, 0, i);
+            return true;
+        }
+        
+        // ReSharper disable once MemberCanBePrivate.Global
+        [Required]
+        public string SourceFile { get; set; }
+        [Required]
+        // ReSharper disable once MemberCanBePrivate.Global
+        public string DestinationFile { get; set; }
+    }
+}
